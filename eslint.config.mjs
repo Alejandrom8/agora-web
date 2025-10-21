2d3caa4d639f17cd1c768eebd11c7d601ef3c3d4
Alejandro Gomez <alejandro.gomez.dev@gmail.com>
2025-10-21 17:05:17 -0600

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,10 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:@typescript-eslint/recommended'),
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-    extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
     },
