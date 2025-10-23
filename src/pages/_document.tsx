bbb885ef183c0bb3f7b7d1d99b907377bc3b510b
Alejandro Gomez <alejandro.gomez.dev@gmail.com>
2025-10-23 16:49:26 -0600

    feat: update document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ import Document, {
   DocumentInitialProps,
 } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
-import createEmotionCache from '../createEmotionCache';
+import createEmotionCache from '../lib/createEmotionCache';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
